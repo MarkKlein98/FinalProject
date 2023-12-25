7dd9831dcfed62b2051dd90186eab0e23270d2d7
Shalev <shalevaharon125@gmail.com>
2023-12-25 10:56:50 +0200

    Updated Version 20/12/23

--- actions/robot_get_code.py
+++ actions/robot_get_code.py
@@ -12,15 +12,15 @@ phone_Lior = 'Insert phone here'
 phone_Aviv = 'Insert phone here'
 
 
+# --------------------------
 # Function to create MongoDB connection
-
 def create_mongo_connection(user_name, encoded_password, db_name):
     # Connect to MongoDB using the given credentials and return the database object
     client = pymongo.MongoClient(
         f"mongodb+srv://{user_name}:{encoded_password}@cluster0.qnr3p.mongodb.net/{db_name}?retryWrites=true&w=majority&tlsAllowInvalidCertificates=true")
     return client
 
-
+# --------------------------
 # Function to test the MongoDB connection
 def test_connection(db):
     try:
@@ -29,20 +29,17 @@ def test_connection(db):
     except Exception as e:
         print("Error connecting to MongoDB:", e)
 
-
+# --------------------------
 def create_mongo_db(client, db_name):
     # Connect to MongoDB using the given credentials and return the database object
     db = client[db_name]
     return db
 
-
+# --------------------------
 def close_connection(client):
     client.close()
 
-
 # --------------------------
-
-
 def get_loginCode(db, phone):
     collection = db['users']
     document = collection.find_one({'phone': phone})
@@ -54,9 +51,7 @@ def get_loginCode(db, phone):
         print('Invalid phone number.')
         return None
 
-
 # --------------------------
-
 # Get code for a desired phone number
 def full():
     client = create_mongo_connection(user_name, encoded_password, db_name)
@@ -64,3 +59,6 @@ def full():
     test_connection(db)
     login_code = get_loginCode(db, phone_Shalev)
     return login_code
+
+# --------------------------
+
